c9940247c71c805be13af3e595e6556421d1ef62
a01639224 <86751053+a01639224@users.noreply.github.com>
2022-11-02 09:13:19 -0600

    Create main.cpp

---
+++ 1.11.22/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int strToInt(std::string input) {
+  std::string numStr;
+  for (char c : input) {
+    if (isdigit(c)) {
+      numStr += c;
+    }
+    if (c == ',') {
+      return stoi(numStr);
+    }
+    if (c == ')') {
+      return stoi(numStr);
+    }
+  }
+  return stoi(numStr);
+}
+
+int main() {
+  std::string sX1, sY1, sX2, sY2;
+  int x1, y1, x2, y2;
+
+  std::cin >> sX1;
+  std::cin >> sY1;
+  std::cin >> sX2;
+  std::cin >> sY2;
+
+  x1 = strToInt(sX1);
+  y1 = strToInt(sY1);
+  x2 = strToInt(sX2);
+  y2 = strToInt(sY2);
+
+  int dx = x2 - x1;
+  int dy = y2 - y1;
+
+  std::cout << std::endl;
+  int i = 1;
+  if (x1 > x2) {
+    std::swap(x1, x2);
+    std::swap(y1, y2);
+  }
+
+  for (int x = x1; x < x2; x = x + i) {
+    int y = y1 + dy * (x - x1) / dx;
+    std::cout << "(" << x << ", " << y << ")" << std::endl;
+  }
+}
